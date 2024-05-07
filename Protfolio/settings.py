2587189cab1f450b02a504f79ee9a003e33b6ad5
sagorluc <mdsagorluc@gmail.com>
2024-05-07 21:04:59 +0600

    update

--- Protfolio/settings.py
+++ Protfolio/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = BASE_DIR/ 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_DIRS = [STATIC_DIR,]
 
